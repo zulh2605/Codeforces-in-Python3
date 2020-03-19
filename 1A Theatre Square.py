782f329d7ec5ab588e0068e9fea95e0c99278f0e
Zulhafiz Zulkifli <zulh2605@gmail.com>
2020-03-19 16:54:27 +0800

    better solution to 1A

--- 1A Theatre Square.py
+++ 1A Theatre Square.py
@@ -6,15 +6,8 @@ n,m,a=input().strip().split()
 n=int(n)
 m=int(m)
 a=int(a)
-nCount=0
-mCount=0
 
-while n>0:
-    n-=a
-    nCount+=1
-
-while m>0:
-    m-=a
-    mCount+=1
+nCount=math.ceil(n/a)
+mCount=math.ceil(m/a)
 
 print(nCount*mCount)
